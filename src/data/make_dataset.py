214a8377bbe10f442015b8ffd31854d131d31579
Arturo Polanco Lozano <apolanco3225@gmail.com>
2024-03-05 11:11:01 -0500

    inform testing using logger

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,6 +1,17 @@
 
-import pandas as pd
 import os
+import sys
+import logging
+import pandas as pd
+
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s:%(name)s: %(message)s",
+    level=logging.INFO, 
+    datefmt="%H:%M:%S",
+    stream=sys.stderr
+)
+
+logger = logging.getLogger(__name__)
 
 
 # current_working_directory = os.getcwd()
@@ -12,7 +23,6 @@ import os
 
 raw_data_folder  = "data/raw"
 
-
 movies_data_path  = os.path.join(raw_data_folder, "movies.csv")
 financial_data_path = os.path.join(raw_data_folder, "financials.csv")
 opening_data_path = os.path.join(raw_data_folder, "opening_gross.csv")
@@ -20,17 +30,20 @@ opening_data_path = os.path.join(raw_data_folder, "opening_gross.csv")
 file_paths = [movies_data_path, financial_data_path, opening_data_path]
 trouble_files = []
 
+logging.info("Testing Data Paths.")
+
 for path in file_paths:
 
     try:
         data = pd.read_csv(path)
     except:
         trouble_files.append(path)
-        raise(f"An error has occurred while reading {path}")
+        logging.error(f"An error has occurred while reading {path}")
     
     finally:
-        print(f"File located in {path} read successfully.")
+        logging.info(f"File located in {path} read successfully.")
+
 
 if len(trouble_files) > 0:
-    print(f"The following paths couldn't be read: {trouble_files}")
+    logging.error(f"The following paths couldn't be read: {trouble_files}")
 
