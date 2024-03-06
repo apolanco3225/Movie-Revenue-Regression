9b09339ab2340aa2cdb06d46df6768482ddf43a6
Arturo Polanco Lozano <apolanco3225@gmail.com>
2024-03-06 11:21:58 -0500

    store all util functions for inference in this file

---
+++ api/app/utils.py
@@ -0,0 +1,38 @@
+import os
+import joblib
+import numpy as np
+import pandas as pd
+from io import BytesIO
+from pydantic import BaseModel
+from sklearn.pipeline import Pipeline
+from .schemas import PredictionRequest
+
+
+
+def get_model(
+        model_folder = "models/", 
+        model_name = "model.pkl"
+    ) -> Pipeline:
+
+    full_model_folder = os.path.join(model_folder, model_name)
+    model = joblib.load(full_model_folder)
+    
+    return model 
+
+def transform_to_array(class_model = BaseModel) -> pd.DataFrame:
+    data_dict = {key:[value] for key, value in class_model.dict().items()}
+    data_df  = pd.DataFrame.from_dict(data_dict)
+    data_array = data_df.values
+
+    return data_array    
+
+
+
+model = get_model()
+
+def get_prediction(request: PredictionRequest):
+    data = transform_to_array(request)
+    prediction  = model.predict(data)[0]
+    return max(0, prediction)
+    # return data
+    # return data
