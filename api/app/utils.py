ed82be00c60d8992680f50e1fda59f6423f42d6f
Arturo Polanco Lozano <apolanco3225@gmail.com>
2024-03-15 12:02:57 -0500

    update default model path

--- api/app/utils.py
+++ api/app/utils.py
@@ -8,31 +8,24 @@ from sklearn.pipeline import Pipeline
 from .schemas import PredictionRequest
 
 
-
 def get_model(
+        # model_folder = "../models/", 
         model_folder = "models/", 
         model_name = "model.pkl"
     ) -> Pipeline:
-
     full_model_folder = os.path.join(model_folder, model_name)
     model = joblib.load(full_model_folder)
-    
     return model 
 
 def transform_to_array(class_model = BaseModel) -> pd.DataFrame:
     data_dict = {key:[value] for key, value in class_model.dict().items()}
     data_df  = pd.DataFrame.from_dict(data_dict)
     data_array = data_df.values
-
     return data_array    
 
 
-
-model = get_model()
-
 def get_prediction(request: PredictionRequest):
     data = transform_to_array(request)
+    model = get_model()
     prediction  = model.predict(data)[0]
     return max(0, prediction)
-    # return data
-    # return data
